8052688941cd57fc2f1f9a60b4f0d0e23e680012
Ak <abdallahakrab98@gmail.com>
2021-05-19 15:52:28 +0200

    Video 2: Nodes

---
+++ Assets/Scripts/BehaviourTree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTree : Node
+{
+
+    public BehaviourTree()
+    {
+        name = "Tree";
+    }
+    public BehaviourTree(string name)
+    {
+        this.name = name;
+    }
+
+    public void PrintTree()
+    {
+        Print(this);
+    }
+
+    private void Print(Node node)
+    {
+        if (node == null)
+        {
+            return;
+
+        }
+        Debug.Log(node.name);
+        if (node.children.Count == 0)
+        {
+            return;
+        }
+        foreach (Node child in node.children)
+        {
+            Print(child);
+        }
+
+    }
+}
---
+++ Assets/Scripts/RobberBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobberBehaviour : MonoBehaviour
+{
+    BehaviourTree tree;
+    void Start()
+    {
+        // Cpnstructing tree
+        tree = new BehaviourTree();
+        Node steal = new Node("Steal Something");
+        Node goToDiamond = new Node("Go to Diamond");
+        Node goToVan = new Node("Go to Van");
+
+        steal.AddChild(goToDiamond);
+        steal.AddChild(goToVan);
+        tree.AddChild(steal);
+        tree.PrintTree();
+
+    }
+
+    void Update()
+    {
+
+    }
+}
